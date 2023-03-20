62186f3f22fef3cb1666d5e3676ca885d46da73a
Your Name <bozo.bakula@gmail.com>
2023-03-20 19:50:12 +0100

    first commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../components/Layout/Header'
+import Hero from '../components/Route/Hero/Hero'
+import Categories from '../components/Route/Categories/Category'
+import BestDeals from '../components/Route/BestDeals/BestDeals'
+import FeaturedProducts from '../components/Route/FeaturedProduct/FeaturedProducts'
+import Events from '../components/Events/Events'
+import Sponsored from '../components/Route/Sponsored'
+import Footer from '../components/Layout/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header activeHeading={1} />
+      <Hero />
+      <Categories />
+      <BestDeals />
+      <Events />
+      <FeaturedProducts />
+      <Sponsored />
+      <Footer />
+    </div>
+  )
+}
+
+export default HomePage
